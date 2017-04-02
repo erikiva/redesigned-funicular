16b7612d198b474c0fc8b66610b3e45dcc29c6bd
natalia <nataliav@gmail.com>
2017-04-03 00:38:45 +0200

    Inicial schema and resolver commit

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Video {
+    id: ID,
+    title: String,
+    duration: Int,
+    watched: Boolean
+  }
+
+  type Query {
+    video: Video
+    videos: [Video]
+  }
+  
+  type Schema {
+    query: Query
+  }
+`);
+
+const videoA = {
+  id: 'a',
+  title: 'Create a GRaphQL Schema',
+  duration: 120,
+  watched: true
+}
+
+const videoB = {
+  id: 'b',
+  title: 'Create a GRaphQL Query',
+  duration: 135,
+  watched: false
+}
+
+const videos = [videoA, videoB];
+
+const resolvers = {
+  video:() => ({
+    id: () => '1',
+    title: () => 'bar',
+    duration: () => 180,
+    watched: () => true,
+  }),
+  videos: () => videos,
+}
+
+const query = `
+  query myFirstQuery{
+    videos {
+      id,
+      title,
+      duration,
+      watched
+    }
+  }
+`;
+
+graphql(schema, query, resolvers)
+  .then(result => {console.log(result)})
+  .catch(error => {console.log(error)});
